5b8f8cb4bf5f20401072a79e0ebbb83d03240131
adallman2 <adallman2@wisc.edu>
2019-10-07 14:07:46 -0500

    SQL code in case db-fiddle deletes it

--- backend.js
+++ backend.js
@@ -1,3 +1,4 @@
+//db-fiddle.com/f/t18DNCzRaR3hmLkuPVM4Sd/0
 var request = require("request");
 
 module.exports = {
@@ -5,7 +6,7 @@ module.exports = {
     getZipInfo : function(zipcode) {
 
         var options = { method: 'GET',
-        url: 'http://www.zipcodeapi.com/rest/5pBEn5IMfU0ZVOClCd979kkirCTDo6y76loU7Ur0K0CFx387WYqKAQ1bEeITlb6k/info.json/' + zipcode + '/degrees',
+        url: 'http://www.zipcodeapi.com/rest/6mW7gW5KQYV2X0GBwhf0phgli4bOihjIAVJWTca6yo0G06xTybWs75vjWm0aqC1J/info.json/' + zipcode + '/degrees',
         };
 
         request(options, function (error, response, body) {
