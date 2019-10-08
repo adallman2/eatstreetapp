270377f219d2d7a120d3758de2e15b9a5a530452
adallman2 <adallman2@wisc.edu>
2019-10-08 15:46:10 -0500

    added bootstrap and dependencies / updated import statements

---
+++ dallman-eatstreet/src/components/ZipSearch.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class ZipSearch.js extends Component {
+   
+    constructor() {
+        super();
+        this.state = {
+            results: {}
+        }
+    }
+}
