6bad7f3c93b23c9fd072edb6d52af66cc48fabf1
adallman2 <adallman2@wisc.edu>
2019-10-09 20:55:35 -0500

    reformated some text

--- dallman-eatstreet/src/components/ZipData.js
+++ dallman-eatstreet/src/components/ZipData.js
@@ -19,7 +19,7 @@ class ZipData extends React.Component {
                     <h6 class="card-subtitle mb-2 text-muted">{this.props.city}, {this.props.st}</h6>
                     <p class="card-text">Latitude: {this.props.lat}</p>
                     <p class="card-text">Longitude: {this.props.long}</p>
-                    <p class="card-text">Timezone: {this.props.timezone}({this.props.timezoneAbbr}) Offset(UTC): {this.props.offset}</p>
+                    <p class="card-text">Timezone: {this.props.timezone}({this.props.timezoneAbbr}) | Offset(UTC): {this.props.offset}</p>
                     <p class="card-text">Other Names: {this.props.oNames != null ? this.props.oNames : "none"}</p>
                     <p class="card-text">Area Codes: {this.props.areaCodes}</p>
                 </div>
